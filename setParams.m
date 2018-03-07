78d3b4c4bae5ffda7e2bca47b3dc349c3fef67e4
Kim Speck <kimspeck@Sven-Hohensteins-MacBook-Air.local>
2018-03-07 12:13:44 +0100

    trialListGen function

--- setParams.m
+++ setParams.m
@@ -2,8 +2,9 @@ function [design] = setParams()
     design=struct();
     %% General
     design.vp=input('Versuchsperson Nr: ')
-    %% Times
-    design.presTime=5;
+    %% Trials 
+    design.nTrials=12;
+    design.p_correct = .5 % 50% percent identical cue and target location
     %% Colors
     design.bgCol=[0.9,0.9,0.9]
     design.FCsize=50;
--- trialListGen.m
+++ trialListGen.m
@@ -1,12 +1,22 @@
 function [trialList] = trialListGen(design)
-
 % Generate Design Matrix
 % Die Matrix braucht
 % Trialnummer
+    TrialNr = 1:design.nTrials
+    
 % Kondition 1: Zeit zwischen cue und target= 0.2,0.5,1s
-% Kondition 2: Seite auf der der cue auftaucht = links, rechts
+    ct_interval = repelem([0.2,0.5,1], design.nTrials/3)
+    ct_interval = ct_interval(randperm(length(ct_interval)))
+    
+% Kondition 2: Seite auf der der cue auftaucht: links = 0, rechts = 1
+    c_location = repelem([0,1], design.nTrials/2)
+    c_location = c_location(randperm(length(c_location)))
+
 % Kondition 3: Seite auf der der target auftaucht (soll 50% richtig sein)
+    location_change = randperm(length(c_location), design.nTrials*design.p_correct)
+    t_location = c_location
+    t_location(location_change) = ~c_location(location_change)
 
-
+    trialList = horzcat(TrialNr', ct_interval', c_location', t_location')
 end
 
